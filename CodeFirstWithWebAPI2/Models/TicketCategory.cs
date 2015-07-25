1ff5ba2c78021125f82c8a5d2343735c0f6f3455
cyricx <contact@chrisbyram.com>
2015-07-25 17:13:08 -0500

    2.2 Add Models for Ticket and TicketCategory

---
+++ CodeFirstWithWebAPI2/Models/TicketCategory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstWithWebAPI2.Models
+{
+    public class TicketCategory
+    {
+        [Key]
+        public int TicketCategoryID { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
+
+        //Relationships
+        //ICollection is used to represent a category has many tickets.
+        public virtual ICollection<Ticket> Tickets { get; set; }
+    }
+}
