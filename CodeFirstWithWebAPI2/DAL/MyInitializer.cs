9f1690ddf7b9a1c973c4e5fb7254fbbdd832784e
cyricx <contact@chrisbyram.com>
2015-07-25 19:59:18 -0500

    5.1 Add Initializer

---
+++ CodeFirstWithWebAPI2/DAL/MyInitializer.cs
@@ -0,0 +1,26 @@
+﻿using CodeFirstWithWebAPI2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstWithWebAPI2.DAL
+{
+    public class MyInitializer : DropCreateDatabaseAlways<MyContext>
+    {
+        protected override void Seed(MyContext context)
+        {
+            List<TicketCategory> categories = new List<TicketCategory>()
+            {
+                new TicketCategory { Name = "Feature", IsActive = true },
+                new TicketCategory { Name = "Enhancement", IsActive = true },
+                new TicketCategory { Name = "Bug", IsActive = true }
+            };
+            categories.ForEach(x => context.TicketCategories.Add(x));
+            context.SaveChanges();
+
+            base.Seed(context);
+        }
+    }
+}
